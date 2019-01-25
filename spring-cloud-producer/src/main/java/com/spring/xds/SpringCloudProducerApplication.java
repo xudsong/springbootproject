97c5d6a4ef19c01d79265597dd6b096533efeba5
song <xudsong@163.com>
2019-01-25 16:14:23 +0800

    服务生产者

---
+++ spring-cloud-producer/src/main/java/com/spring/xds/SpringCloudProducerApplication.java
@@ -0,0 +1,14 @@
+package com.spring.xds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudProducerApplication.class, args);
+	}
+
+}
+
