f60eb89027a13dea8ff473f6828c886d1cfe5400
song <xudsong@163.com>
2019-01-27 14:12:39 +0800

    微服务服务生产者

---
+++ spring-cloud-producer/src/main/java/com/spring/xds/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.spring.xds.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @PostMapping(value = "/hello")
+    public String sayHello(@RequestParam String name){
+        return "hello "+name+"，this is first messge";
+    }
+}
